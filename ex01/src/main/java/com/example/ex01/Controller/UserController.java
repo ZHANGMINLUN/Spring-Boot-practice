18afeff5a1215d6a7ed7d7cc23fac627cff3b03d
ZHANGMINLUN <allen5410323@gmail.com>
2022-03-12 22:20:07 +0800

    1/3-getHTML with thymeleaf (@Controller)

--- ex01/src/main/java/com/example/ex01/Controller/UserController.java
+++ ex01/src/main/java/com/example/ex01/Controller/UserController.java
@@ -5,12 +5,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/api")
 public class UserController {
     @GetMapping("/hello")
     public String sayhello(){
         return "HelloWorld!";
     }
+
+    @GetMapping("/getHTML")
+    public String sendHTML(){
+        return "book";
+    }
+
 
 }
