208c9f23400d0b2138193565cf3d97b0cedf14f3
ZHANGMINLUN <allen5410323@gmail.com>
2022-03-12 22:11:56 +0800

    1/3-HelloWorld

---
+++ ex01/src/main/java/com/example/ex01/Controller/UserController.java
@@ -0,0 +1,16 @@
+package com.example.ex01.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class UserController {
+    @GetMapping("/hello")
+    public String sayhello(){
+        return "HelloWorld!";
+    }
+
+}
