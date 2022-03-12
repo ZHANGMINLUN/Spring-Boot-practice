2f501f0a2b49ad0cdc5056e80755fead570dd03e
ZHANGMINLUN <allen5410323@gmail.com>
2022-03-12 22:35:37 +0800

    1/3-getMap with @ResponseBody

--- ex01/src/main/java/com/example/ex01/Controller/UserController.java
+++ ex01/src/main/java/com/example/ex01/Controller/UserController.java
@@ -3,11 +3,16 @@ package com.example.ex01.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/api")
 public class UserController {
+
     @GetMapping("/hello")
     public String sayhello(){
         return "HelloWorld!";
@@ -16,6 +21,15 @@ public class UserController {
     @GetMapping("/getHTML")
     public String sendHTML(){
         return "book";
+    }
+
+    @GetMapping("/getMap")
+    @ResponseBody
+    public Object sendMap(){
+        Map<String, Object> map = new HashMap<>();
+        map.put("name", "hello");
+        map.put("age", 18);
+        return map;
     }
 
 
