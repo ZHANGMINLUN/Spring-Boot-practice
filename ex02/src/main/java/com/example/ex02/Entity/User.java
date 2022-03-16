deddd43e7a89573c3dc971315b33cb3af9da753f
ZHANGMINLUN <allen5410323@gmail.com>
2022-03-16 18:58:35 +0800

    2/3-udemy UserConverter @Data fixed

--- ex02/src/main/java/com/example/ex02/Entity/User.java
+++ ex02/src/main/java/com/example/ex02/Entity/User.java
@@ -1,10 +1,13 @@
 package com.example.ex02.Entity;
 
+import lombok.Data;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
+@Data
 public class User {
 
     @Id
@@ -19,45 +22,7 @@ public class User {
 
     }
 
-    public long getId() {
-        return id;
-    }
 
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public int getPhonenumber() {
-        return phonenumber;
-    }
-
-    public void setPhonenumber(int phonenumber) {
-        this.phonenumber = phonenumber;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 
     @Override
     public String toString() {
