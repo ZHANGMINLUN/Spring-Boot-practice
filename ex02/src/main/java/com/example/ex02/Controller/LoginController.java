040bfe8bcf0c52ee46e7ac73bfa8080a412effca
ZHANGMINLUN <allen5410323@gmail.com>
2022-03-17 11:42:41 +0800

    2/3-Hibernate Validation

--- ex02/src/main/java/com/example/ex02/Controller/LoginController.java
+++ ex02/src/main/java/com/example/ex02/Controller/LoginController.java
@@ -1,16 +1,17 @@
 package com.example.ex02.Controller;
 
 import com.example.ex02.Entity.User;
-import com.example.ex02.Entity.UserRepository;
 import com.example.ex02.Form.UserForm;
 import com.example.ex02.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import java.awt.print.Book;
+import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 public class LoginController {
@@ -19,20 +20,33 @@ public class LoginController {
     private UserService userService;
 
     @GetMapping("/registerPage")
-    public String registerPage(){
+    public String registerPage() {
         return "registerPage";
     }
 
     @GetMapping("/loginPage")
-    public String loginPage(){
+    public String loginPage() {
         return "loginPage";
     }
 
     @PostMapping("/register")
-    public String POSTregister(UserForm userForm){
-        User user = userForm.convertToUser();
+    public String POSTregister(@Valid User user, BindingResult result) {
+        if(!user.getPassword().equals(user.getConfirmPassword())){
+            result.rejectValue("confirmPassword","Password must be the same","PasswordMismatch");
+        }
+
+        if (result.hasErrors()) {
+            List<FieldError> fieldErrors = result.getFieldErrors();
+            for (FieldError error : fieldErrors) {
+                System.out.println("getField - " + error.getField());
+                System.out.println("getDefaultMessage - " + error.getDefaultMessage());
+                System.out.println("getCode - " + error.getCode());
+            }
+            return "registerPage";
+        }
+
         userService.AddUserInformation(user);
-        return "loginPage";
+        return "redirect:/loginPage";
     }
 
 }
--- ex02/src/main/java/com/example/ex02/Entity/User.java
+++ ex02/src/main/java/com/example/ex02/Entity/User.java
@@ -1,35 +1,44 @@
 package com.example.ex02.Entity;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
-
-import javax.persistence.*;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Transient;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
 
 @Entity
 @Data
 public class User {
 
+    public static final String PHONE_REG = "^((13[0-9])|(15[^4])|(18[0,2,3,5-9])|(17[0-8])|(147))\\d{8}$";
+
     @Id
     @GeneratedValue
     private long id;
+
+    @NotNull
     private String username;
+
+    @NotNull
+    @Length(min = 6, message = "unless 6 number")
     private String password;
-    private int phonenumber;
+
+//    @Pattern(regexp = PHONE_REG, message = "Please enter right number")
+    private String phonenumber;
+
+    @Email
     private String email;
 
     @Transient
+    @NotBlank
     private String confirmPassword;
-
-
-    public User(){
-
-    }
-
-
 
     @Override
     public String toString() {
